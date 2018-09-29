ad7b7061f8b1fd68d3fead0d3f4e98a9787c108c
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.net>
2018-09-29 19:49:02 +0200

    Reorganise functions and variables, and update dialog box

--- chapter_skipper.lua
+++ chapter_skipper.lua
@@ -32,28 +32,26 @@ function descriptor()
 	}
 end
 
--- local config = require "chapter_skipper_config"
-
-keep_checking = true --to disable the check loop on deactivation
-chapterToSkip = -1
-
 --Run on plugin activation
 function activate()
 	vlc.playlist.pause()
 	d = vlc.dialog("Chapter Skipper")
 	d:show()
-	d:add_label( "This extension will periodically check the chapter your video is at, with the first chapter being 0. ", 2, 1, 1, 1 )
-	d:add_label( "It cause VLC to become unresponsive, but it will return to normal after the skip.", 2, 2, 1, 1 )
+	d:add_label( "This extension will continually check the chapter your video is at, with the first chapter being 0. ", 2, 1, 1, 1 )
+	d:add_label( "It causes VLC to become unresponsive.", 2, 2, 1, 1 )
 	d:add_label( "You can move the 'extension not responding' screen to another monitor to minimize viewing impact.", 2, 3, 1, 1 )
 
-	fullscreenOption = d:add_check_box( "Go to fullscreen", true, 2, 4, 1, 1)
-	restartOption = d:add_check_box( "Restart video", true, 2, 5, 1, 1)
+	fullscreenOption = d:add_check_box( "Go to fullscreen", true, 2, 4, 1, 1 )
+	restartOption = d:add_check_box( "Restart video", true, 2, 5, 1, 1 )
 
-	d:add_label( "Chapter # to skip", 1, 1, 1, 1 )
+	d:add_label( "* Chapter indices are separated by commas (,) and can be negative indicating reverse ordering with -1 being the last chapter.", 2, 6, 1, 1 )
+	d:add_label( "Chapter indices* to skip:", 1, 1, 1, 1 )
 	chapterToSkipInput = d:add_text_input( 1, 1, 2, 1, 1 )
 	d:add_button("Enable", click_Start, 1, 3, 1, 1)
 	d:add_button("Disable", click_Disable, 1, 4, 1, 1)
 end
+
+chapterToSkip = -1
 
 --Verify input options and enter check loop
 function click_Start()
@@ -76,12 +74,7 @@ function click_Start()
 	start_checker()
 end
 
---Disable check loop.
-function click_Disable()
-	d:hide()
-	vlc.msg.info("Disabled chapter skipper")
-	deactivate()
-end
+keep_checking = true --to disable the check loop on deactivation
 
 --Start chapter skip loop
 function start_checker()
@@ -92,14 +85,6 @@ function start_checker()
 	while keep_checking do
 		chapter_skip_check()
 	end
-
-	vlc.msg.info("Ending chapter skipper")
-end
-
---Run on plugin deactivation
-function deactivate()
-	keep_checking = false
-	vlc.msg.info("Deactivated chapter skipper")
 end
 
 -- If the current chapter matches the one we want to skip, skip it.
@@ -109,7 +94,6 @@ function chapter_skip_check()
 	if get_chapter() == chapterToSkip then
 		vlc.msg.info("Skipping chapter")
 		skip_chapter()
-		deactivate()
 	end
 end
 
@@ -127,6 +111,19 @@ end
 function titlechap_offset(var,offset)
     local input = vlc.object.input()
     vlc.var.set( input, var, vlc.var.get( input, var ) + offset )
+end
+
+--Disable check loop.
+function click_Disable()
+	d:hide()
+	vlc.msg.info("Disabled chapter skipper")
+	deactivate()
+end
+
+--Run on plugin deactivation
+function deactivate()
+	keep_checking = false
+	vlc.msg.info("Deactivated chapter skipper")
 end
 
 function meta_changed()
