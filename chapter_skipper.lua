2ee443ecd5de3329e964db358fade1843bedc750
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.net>
2018-09-29 15:14:49 +0200

    Add indentation

--- chapter_skipper.lua
+++ chapter_skipper.lua
@@ -21,15 +21,15 @@ LICENSE:
 --]]
 
 function descriptor()
-return {
-title = "Chapter Skipper",
-author = "Victor Pascu <victor.pscu@gmail.com>",
-version = 0.1,
-shortdesc = 'Chapter Skipper',
-url = 'https://github.com/VictorPascu',
-description = "This extension will skip a specified chapter when your video reaches it.",
-capabilities = {"input-listener"}
-}
+	return {
+		title = "Chapter Skipper",
+		author = "Victor Pascu <victor.pscu@gmail.com>",
+		version = 0.1,
+		shortdesc = 'Chapter Skipper',
+		url = 'https://github.com/VictorPascu',
+		description = "This extension will skip a specified chapter when your video reaches it.",
+		capabilities = {"input-listener"}
+	}
 end
 
 keep_checking = true --to disable the check loop on deactivation
@@ -38,63 +38,63 @@ chapterToSkip = -1
 
 --Run on plugin activation
 function activate()
-vlc.playlist.pause()
-d = vlc.dialog("Chapter Skipper")
-d:show()
-d:add_label( "This extension will periodically check the chapter your video is at, with the first chapter being 0. ", 2, 1, 1, 1 )
-d:add_label( "It cause VLC to become unresponsive, but it will return to normal after the skip.", 2, 2, 1, 1 )
-d:add_label( "You can move the 'extension not responding' screen to another monitor to minimize viewing impact.", 2, 3, 1, 1 )
+	vlc.playlist.pause()
+	d = vlc.dialog("Chapter Skipper")
+	d:show()
+	d:add_label( "This extension will periodically check the chapter your video is at, with the first chapter being 0. ", 2, 1, 1, 1 )
+	d:add_label( "It cause VLC to become unresponsive, but it will return to normal after the skip.", 2, 2, 1, 1 )
+	d:add_label( "You can move the 'extension not responding' screen to another monitor to minimize viewing impact.", 2, 3, 1, 1 )
 
-fullscreenOption = d:add_check_box( "Go to fullscreen", true, 2, 4, 1, 1)
-restartOption = d:add_check_box( "Restart video", true, 2, 5, 1, 1)
+	fullscreenOption = d:add_check_box( "Go to fullscreen", true, 2, 4, 1, 1)
+	restartOption = d:add_check_box( "Restart video", true, 2, 5, 1, 1)
 
-d:add_label( "Chapter # to skip", 1, 1, 1, 1 )
-chapterToSkipInput = d:add_text_input( 1, 1, 2, 1, 1 )
-d:add_button("Enable", click_Start,1,3,1,1)
-d:add_button("Disable", click_Disable,1,4,1,1)
+	d:add_label( "Chapter # to skip", 1, 1, 1, 1 )
+	chapterToSkipInput = d:add_text_input( 1, 1, 2, 1, 1 )
+	d:add_button("Enable", click_Start,1,3,1,1)
+	d:add_button("Disable", click_Disable,1,4,1,1)
 end
 
 --Verify input options and enter check loop
 function click_Start()
-vlc.msg.info("Enabling chapter skipper.")
-chapterToSkip = tonumber(chapterToSkipInput:get_text())
-vlc.msg.info("Target chapter:")
-vlc.msg.info(chapterToSkip)
-vlc.playlist.play()
+	vlc.msg.info("Enabling chapter skipper.")
+	chapterToSkip = tonumber(chapterToSkipInput:get_text())
+	vlc.msg.info("Target chapter:")
+	vlc.msg.info(chapterToSkip)
+	vlc.playlist.play()
 
-if restartOption:get_checked() then
-vlc.var.set(vlc.object.input(), "time", 0)
-end
+	if restartOption:get_checked() then
+		vlc.var.set(vlc.object.input(), "time", 0)
+	end
 
-if fullscreenOption:get_checked() then
-	vlc.video.fullscreen()
-end	
+	if fullscreenOption:get_checked() then
+		vlc.video.fullscreen()
+	end	
 
-d:hide()
+	d:hide()
 
-start_checker()
+	start_checker()
 end
 
 --Disable check loop.
 function click_Disable()
-d:hide()
-vlc.msg.info("Disabled chapter skipper.")
-deactivate()
+	d:hide()
+	vlc.msg.info("Disabled chapter skipper.")
+	deactivate()
 end
 
 --Start chapter skip loop
 function start_checker()
-keep_checking = true
+	keep_checking = true
 
-vlc.msg.info("Running chapter skipper")
+	vlc.msg.info("Running chapter skipper")
 
-recheck_chapter_skip()
+	recheck_chapter_skip()
 end
 
 --Run on plugin deactivation
 function deactivate()
-keep_checking = false
-vlc.msg.info("Deactivated chapter skipper.")
+	keep_checking = false
+	vlc.msg.info("Deactivated chapter skipper.")
 end
 
 --[[
@@ -109,46 +109,46 @@ a while.
 --]]
 function sleep(s)
   if keep_checking == true then
-  vlc.msg.info("Sleeping...")
+  	vlc.msg.info("Sleeping...")
   
-  local ntime = os.time() + s
-  repeat until os.time() > ntime
-  
-  vlc.msg.info("Sleep done...")
+	local ntime = os.time() + s
+	repeat until os.time() > ntime
+	
+	vlc.msg.info("Sleep done...")
   end
 end
 
 --Will re-run check between sleeps.
 function recheck_chapter_skip()
-if keep_checking then
-	vlc.msg.info("Rechecking chapter skip...")
-	sleep(sleep_duration)
-	chapter_skip_check()
-end
+	if keep_checking then
+		vlc.msg.info("Rechecking chapter skip...")
+		sleep(sleep_duration)
+		chapter_skip_check()
+	end
 end
 
 -- If the current chapter matches the one we want to skip, skip it.
 -- There's room for improvement here with chapter lists (instead of just one),
 -- but that's probably for a future version.
 function chapter_skip_check()
-if get_chapter() == chapterToSkip then
-	vlc.msg.info("Skipping chapter")
-	skip_chapter()
-	deactivate()
+	if get_chapter() == chapterToSkip then
+		vlc.msg.info("Skipping chapter")
+		skip_chapter()
+		deactivate()
 	else 
-	vlc.msg.info("Not at target chapter")
-	recheck_chapter_skip()
+		vlc.msg.info("Not at target chapter")
+		recheck_chapter_skip()
 	end
 end
 
 -- Get chapter number
 function get_chapter()
-return vlc.var.get(vlc.object.input(), "chapter") 	
+	return vlc.var.get(vlc.object.input(), "chapter") 	
 end
 
 -- Skip wrapper
 function skip_chapter()
-titlechap_offset('chapter', 1)
+	titlechap_offset('chapter', 1)
 end
 
 -- VLC's function for changing chapters
