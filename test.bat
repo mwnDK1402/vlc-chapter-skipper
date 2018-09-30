ba3d5c072d05b11768c2400b74cca2dc82dd654b
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.net>
2018-09-30 11:49:13 +0200

    Add VSCode-specific files

---
+++ test.bat
@@ -0,0 +1,3 @@
+call install.bat
+cd %ProgramFiles%\VideoLAN\VLC
+vlc.exe E:\Anime\Monogatari\[Coalgirls]_Bakemonogatari_(1920x1080_Blu-ray_FLAC)\[Coalgirls]_Bakemonogatari_01_(1920x1080_Blu-ray_FLAC)_[9787055F].mkv
